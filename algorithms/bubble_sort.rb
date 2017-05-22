17a1cb5fb98c864d716be23c62fcc5c832892d49
Vishal Kalappagari <itzmevishu@gmail.com>
2017-05-23 00:03:12 +0530

    algorithms

---
+++ algorithms/bubble_sort.rb
@@ -0,0 +1,17 @@
+a = [9,8,6,1,2,5,4,3,9,50,12,11]
+elements_count = a.count - 1
+sorted = false
+puts a.count
+puts a.size
+puts a.length
+puts '####'
+while sorted == false do
+  sorted = true
+  (elements_count).times do |i|
+    if a[i+1] < a[i]
+      a[i], a[i+1] = a[i+1], a[i]
+      sorted = false 
+    end 
+  end
+end
+puts a
